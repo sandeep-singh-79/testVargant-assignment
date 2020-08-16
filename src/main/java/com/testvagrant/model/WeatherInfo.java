2e8fcbf3446264fbe610a16662c248c2313df603
Sandeep Singh <sandeep-singh-79@users.noreply.github.com>
2020-08-16 16:10:37 +0530

    screenscapping of Weather info in progress

---
+++ src/main/java/com/testvagrant/model/WeatherInfo.java
@@ -0,0 +1,14 @@
+package com.testvagrant.model;
+
+import java.util.List;
+
+public class WeatherInfo {
+    private String cityName;
+    private List<String> condition;
+    private float windSpeed;
+    private float humidity;
+    private byte tempDegress, tempFahrenheit;
+
+    public WeatherInfo() {
+    }
+}
--- src/test/java/com/testvagrant/pages/NDTVWeatherPO.java
+++ src/test/java/com/testvagrant/pages/NDTVWeatherPO.java
@@ -2,18 +2,18 @@ package com.testvagrant.pages;
 
 import com.testvagrant.base.BasePageObject;
 import com.testvagrant.config.FrameworkConfig;
+import com.testvagrant.model.WeatherInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 import static java.lang.String.format;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfAllElements;
 
 public class NDTVWeatherPO extends BasePageObject {
     @FindBy(id = "searchBox")
@@ -22,6 +22,7 @@ public class NDTVWeatherPO extends BasePageObject {
     private WebElement cityWeatherInfo, city, cityContainer;
     private List<WebElement> citySuggestions;
     private final List<WebElement> citySuggestionCheckboxes;
+    private List<WebElement> weatherConditions;
 
 
     public NDTVWeatherPO searchForCity(final String searchValue, final String cityName) throws InterruptedException {
@@ -29,18 +30,36 @@ public class NDTVWeatherPO extends BasePageObject {
         // wait for the script on page load complete before we continue forward
         Thread.sleep(5000);
         citySearchtxtBox.sendKeys(searchValue);
-        wait.until(ExpectedConditions
-                           .visibilityOfAllElements(citySuggestions = driver.findElements(By.xpath("//*[@id='messages']//div[not(contains(@style, 'none'))]"))));
+        wait.until(visibilityOfAllElements(citySuggestions = driver.findElements(By.xpath("//*[@id='messages']//div[not(contains(@style, 'none'))]"))));
         if (isCitySelectable(cityName)) city.click();
         return this;
     }
 
     public NDTVWeatherPO displayCityWeatherInfo(final String cityName) {
         String cityTempContainerLocator = format(".outerContainer[title='%s']", cityName);
-        cityContainer = wait.until(ExpectedConditions
-                                           .visibilityOf(cityWeatherInfo = driver.findElement(By.cssSelector(cityTempContainerLocator))));
+        cityContainer = wait.until(visibilityOf(cityWeatherInfo = driver.findElement(By.cssSelector(cityTempContainerLocator))));
         cityContainer.click();
         return this;
+    }
+
+    public WeatherInfo storeWeatherInfo(final String cityName) {
+        WebElement weatherInfoContainer = wait.until(visibilityOf(driver.findElement(By.className("leaflet-popup-content"))));
+        weatherConditions = weatherInfoContainer.findElements(By.cssSelector(".leaflet-popup-content spa.heading"));
+        Map<String, String> weatherConditionsMap = populateWeatherConditions(weatherConditions, cityName);
+
+        WeatherInfo weatherInfo = new WeatherInfo();
+
+        return weatherInfo;
+    }
+
+    private Map<String, String> populateWeatherConditions(List<WebElement> weatherConditions, String cityName) {
+        Map<String, String> weatherConditionsMap = new HashMap<>();
+        weatherConditionsMap.put("City", cityName);
+        for (WebElement weatherState : weatherConditions) {
+            String[] splits = weatherState.getText().split(":");
+            weatherConditionsMap.put(splits[0].trim(), splits[1]);
+        }
+        return weatherConditionsMap;
     }
 
     private boolean isCitySelectable(final String cityName) {
