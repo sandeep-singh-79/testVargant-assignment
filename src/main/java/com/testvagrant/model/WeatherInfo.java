993aa7e793b40d7ef27be9cb3d334ff7655ab232
Sandeep Singh <sandeep-singh-79@users.noreply.github.com>
2020-08-17 23:59:57 +0530

    using lombok for generating boilerplate code

--- src/main/java/com/testvagrant/model/WeatherInfo.java
+++ src/main/java/com/testvagrant/model/WeatherInfo.java
@@ -1,10 +1,16 @@
 package com.testvagrant.model;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
+@EqualsAndHashCode
+@ToString
+@Getter(lazy = true)
+@NoArgsConstructor
 public class WeatherInfo {
     private String cityName;
     private List<String> condition;
@@ -13,49 +19,4 @@ public class WeatherInfo {
     private float tempDegrees;
     private float tempFahrenheit;
     private int humidity;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        WeatherInfo that = (WeatherInfo) o;
-
-        return new EqualsBuilder()
-                       .append(windSpeed, that.windSpeed)
-                       .append(windGust, that.windGust)
-                       .append(tempDegrees, that.tempDegrees)
-                       .append(tempFahrenheit, that.tempFahrenheit)
-                       .append(humidity, that.humidity)
-                       .append(cityName, that.cityName)
-                       .append(condition, that.condition)
-                       .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                       .append(cityName)
-                       .append(condition)
-                       .append(windSpeed)
-                       .append(windGust)
-                       .append(tempDegrees)
-                       .append(tempFahrenheit)
-                       .append(humidity)
-                       .toHashCode();
-    }
-
-    @Override
-    public String toString() {
-        return "WeatherInfo{" +
-                       "cityName='" + cityName + '\'' +
-                       ", condition=" + condition +
-                       ", windSpeed=" + windSpeed +
-                       ", windGust=" + windGust +
-                       ", tempDegrees=" + tempDegrees +
-                       ", tempFahrenheit=" + tempFahrenheit +
-                       ", humidity=" + humidity +
-                       '}';
-    }
 }
