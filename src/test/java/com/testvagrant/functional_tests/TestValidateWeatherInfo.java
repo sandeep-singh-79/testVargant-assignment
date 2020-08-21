a88e0340a23c0972402cba81094320da172ca320
Sandeep Singh <sandeep-singh-79@users.noreply.github.com>
2020-08-21 11:59:35 +0530

    using functional interface for comparaing objects

--- src/test/java/com/testvagrant/functional_tests/TestValidateWeatherInfo.java
+++ src/test/java/com/testvagrant/functional_tests/TestValidateWeatherInfo.java
@@ -6,9 +6,6 @@ import com.testvagrant.base.api.ApiBase;
 import com.testvagrant.base.api.EndPoints;
 import com.testvagrant.exception.NoSuchCityException;
 import com.testvagrant.model.WeatherInfo;
-import com.testvagrant.model.comparator.HumidityComparator;
-import com.testvagrant.model.comparator.TemperatureComparator;
-import com.testvagrant.model.comparator.WindSpeedComparator;
 import com.testvagrant.pages.NDTVWeatherPO;
 import com.testvagrant.util.Utils;
 import groovy.json.JsonOutput;
@@ -22,6 +19,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -104,18 +102,51 @@ public class TestValidateWeatherInfo extends BaseTestNGTest {
         RestAssured.reset();
     }
 
+    Comparator<WeatherInfo> tempComparator = ((o1, o2) -> {
+        float absValue = Math.abs(o1.getTempDegrees() - o2.getTempDegrees());
+        float tempVariance = Float.parseFloat(testData.getProperty("tempVariance"));
+        return (absValue >= 0 && absValue <= tempVariance) ? 0 : 1;
+    });
+
+    Comparator<WeatherInfo> humidityComparator = ((o1, o2) -> {
+        float absValue = Math.abs(o1.getHumidity() - o2.getHumidity());
+        float humidityVariance = Float.parseFloat(testData.getProperty("humidityVariance"));
+        return (absValue >= 0 && absValue <= humidityVariance) ? 0 : 1;
+    });
+
+    Comparator<WeatherInfo> windSpeedComparator = ((o1, o2) -> {
+        float absValue = Math.abs(o1.getWindSpeed() - o2.getWindSpeed());
+        float windSpeedVariance = Float.parseFloat(testData.getProperty("windVariance"));
+        return (absValue >= 0 && absValue <= windSpeedVariance) ? 0 : 1;
+    });
+
     @Test
     public void validateCityWeatherInfoIsDisplayed() {
         log.info("Weather data from API: {}", weatherInfoApi.toString());
         log.info("Weather data from Web: {}", weatherInfoWeb.toString());
-        boolean compareTemp = new TemperatureComparator().compare(weatherInfoApi, weatherInfoWeb) == 0;
+        boolean compareTemp = tempComparator.compare(weatherInfoApi, weatherInfoWeb) == 0;
         log.info("Is the temparature within the variance {}? {}", testData.getProperty("tempVariance"), compareTemp);
-        boolean compareHumidity = new HumidityComparator().compare(weatherInfoApi, weatherInfoWeb) == 0;
+        boolean compareHumidity = humidityComparator.compare(weatherInfoApi, weatherInfoWeb) == 0;
         log.info("Is the humidity within the variance {}? {}", testData.getProperty("humidityVariance"), compareHumidity);
-        boolean compareWindSpeed = new WindSpeedComparator().compare(weatherInfoWeb, weatherInfoApi) == 0;
+        boolean compareWindSpeed = windSpeedComparator.compare(weatherInfoWeb, weatherInfoApi) == 0;
         log.info("Is the wind speed within the variance {}? {}", testData.getProperty("windVariance"), compareWindSpeed);
 
         if (compareTemp && compareHumidity && compareWindSpeed) assertTrue(true);
-        else fail("One of the comparators returned a value outside acceptable variance");
+        else {
+            String failureMessage = buildFailureMessage(compareTemp,
+                    compareHumidity,
+                    compareWindSpeed);
+            fail(failureMessage);
+        }
+    }
+
+    private String buildFailureMessage(boolean compareTemp, boolean compareHumidity, boolean compareWindSpeed) {
+        StringBuilder failureMessage = new StringBuilder();
+        failureMessage.append("One or more comparators (");
+        if (!compareTemp) failureMessage.append(" compareTemp");
+        if (!compareHumidity) failureMessage.append(" compareHumidity");
+        if (!compareWindSpeed) failureMessage.append(" compareWindspeed");
+        failureMessage.append("), returned a value outside the acceptable range(s)");
+        return String.valueOf(failureMessage);
     }
 }
